31fcf2801a35c88fe95f2c9e7b1510a00b981f42
Seth Broweleit <setbro@gmail.com>
2015-09-30 11:07:50 -0500

    offtopit

--- topitoff.js
+++ topitoff.js
@@ -1,12 +1,1 @@
-var zz = 0,
-    ee;
-$("body *").each(function() {
-    var e = parseInt($(this).css("z-index"));
-    if (e > zz) {
-        zz = e;
-        ee = $(this);
-    }
-});
-if (ee) {
-    ee.remove();
-}
+var zz=0,ee;$("body *").each(function(){var e=parseInt($(this).css("z-index"));if(e>zz){zz=e;ee=$(this);}});if(ee){ee.remove();$('body').css('overflow','auto');}
